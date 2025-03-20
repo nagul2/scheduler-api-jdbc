bd11c2cacf94009881815e595226d80768ec41e1
nagul2 <hayani0000@gmail.com>
2025-03-20 19:04:23 +0900

    Refactor: [필수 LV1 - 리포지토리 수정 못한 것 추가로 변경]
    1. 메서드명 추가로 변경

--- src/main/java/spring/basic/scheduler/required/model/dto/SchedulerSearchCond.java
+++ src/main/java/spring/basic/scheduler/required/model/dto/SchedulerSearchCond.java
@@ -2,13 +2,16 @@ package spring.basic.scheduler.required.model.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import java.time.LocalDate;
 
 @Getter
 @AllArgsConstructor
 public class SchedulerSearchCond {
-
+	@DateTimeFormat(pattern = "yyyy-MM-dd")
     private LocalDate condDate;
     private String condName;
 
