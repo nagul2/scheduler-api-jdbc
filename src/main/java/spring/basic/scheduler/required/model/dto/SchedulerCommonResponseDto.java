69916416d0714a8c0989427aee82ae2692d6dde4
nagul2 <hayani0000@gmail.com>
2025-03-21 12:24:12 +0900

    Feature: [필수 LV2 - 필요 없는 애노테이션 제거]
    1. 필요 없는 애노테이션 제거

--- src/main/java/spring/basic/scheduler/required/model/dto/SchedulerCommonResponseDto.java
+++ src/main/java/spring/basic/scheduler/required/model/dto/SchedulerCommonResponseDto.java
@@ -2,15 +2,12 @@ package spring.basic.scheduler.required.model.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 /**
  * 일정 생성, 수정 시 사용되는 공통 응답 DTO
  */
-@Setter
+
 @Getter
-@NoArgsConstructor
 @AllArgsConstructor
 public class SchedulerCommonResponseDto {
     private Long id;
