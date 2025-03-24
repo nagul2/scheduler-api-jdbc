84e0eed618e5622b7917b78652b4b852d282a160
nagul2 <hayani0000@gmail.com>
2025-03-24 13:16:28 +0900

    Refactor: [도전 LV3 - URL 매핑 수정]
    1. RequestMapping을 패키지 명과 통일시키도록 /challenge/scheduler로 수정

--- src/main/java/spring/basic/scheduler/challenge/controller/SchedulerControllerV2.java
+++ src/main/java/spring/basic/scheduler/challenge/controller/SchedulerControllerV2.java
@@ -11,7 +11,7 @@ import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/advanced/scheduler")
+@RequestMapping("/challenge/scheduler")
 public class SchedulerControllerV2 {
 
     private final SchedulerService schedulerService;
